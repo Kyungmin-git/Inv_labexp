ed35bc97e40ccbe5a0ab77fe65bc2765e6955004
Kyungmin-git <kkim39@alaska.edu>
2026-03-10 12:12:55 -0800

    Update loading of spect_store_dspongy data

--- Figure_spect/Figure_2c.m
+++ Figure_spect/Figure_2c.m
@@ -2,8 +2,8 @@ load('Spect_store_50ml_new.mat','f_h','spect_store');
 
 spect_store_50=spect_store;
 load('lsponge_noise.mat','spect_store_lsponge_noise');
-load('Spect_store_dspongy.mat','Spect_store');
-spect_store_dspongy50=Spect_store;
+load('Spect_store_dspongy.mat','spect_store_dspongy');
+
 
 figure(1)
 plot(f_h(1:1000),spect_store_50(1,1:1000),'LineWidth',1.5)
@@ -11,7 +11,7 @@ hold on
 plot(f_h(1:1000),spect_store_lsponge_noise(1:1000,1),'Color',[0,0.24,0.54],'LineStyle','--','LineWidth', ...
     1.5)
 plot(f_h(1:1000),spect_store_50(2,1:1000),'LineWidth',1.5,'Color',[0.4660, 0.6740, 0.1880])
-plot(f_h(1:1000),spect_store_dspongy50(1:1000,2),'LineWidth',2,'Color',[0, 0.25, 0])
+plot(f_h(1:1000),spect_store_dspongy(1:1000,2),'LineWidth',2,'Color',[0, 0.25, 0])
 plot(f_h(1:1000),spect_store_50(3,1:1000),'LineWidth',1.5,'Color',[0.8500, 0.3250, 0.0980])
 plot(f_h(1:1000),spect_store_50(4,1:1000),'LineWidth',1.5,'Color',[0.9290, 0.6940, 0.1250])
 
